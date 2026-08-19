8340dfaa6f941a8863c6a3990481dbab5176cbc6
AYU-ANDIKA <124250054@student.upnyk.ac.id>
2026-08-20 06:27:09 +0700

    Refactor login script by cleaning comments and annotations
    Removed redundant comments and updated JSDoc annotations for variables.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,70 +1,45 @@
 /**
  * @module Login-Script
- */
-
-/** 
- * Membuat variabel loginFormElement untuk tampilan form.
- * @constant {HTMLElement}
- * @inner
+ * @type {HTMLElement}
  */
 const loginFormElement = document.querySelector('#loginForm');
 
-/** 
- * Membuat variabel inputEmailElement untuk tampilan input email.
- * @constant {HTMLElement}
- * @inner
+/**
+ * @type {HTMLElement}
  */
 const inputEmailElement = document.querySelector('#inputEmail');
 
-/** 
- * Membuat variabel inputPasswordElement untuk tampilan input password.
- * @constant {HTMLElement}
- * @inner
+/**
+ * @type {HTMLElement}
  */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
-/** 
- * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
- * @constant {string}
- * @inner
+/**
+ * @type {string}
  */
 const expectedEmail = 'admin@dicoding.com';
 
-/** 
- * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
- * @constant {string}
- * @inner
+/**
+ * @type {string}
  */
 const expectedPassword = 'superpassword';
 
-/* Comment : Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
   
-  /** 
-   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
-   * @constant {string}
-   * @inner
+  /**
+   * @type {string}
    */
   const email = inputEmailElement.value;
   
-  /** 
-   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
-   * @constant {string}
-   * @inner
+  /**
+   * @type {string}
    */
   const password = inputPasswordElement.value;
 
-  /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
-
-    /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
-
   } else {
-
-    /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
-
   }
 });
